18d3fbad0ec688b3977f4eee422b9f72a7839fae
Andreas Tennert <andreas-tennert@web.de>
2018-03-25 11:27:17 +0200

    feat: protect against map errors

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -9,13 +9,17 @@ window.initMap = () => {
     if (error) { // Got an error!
       console.error(error);
     } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      try {
+        self.map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: restaurant.latlng,
+          scrollwheel: false
+        });
+        fillBreadcrumb();
+        DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 }
